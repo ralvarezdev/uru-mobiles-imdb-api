7132bc2ec224167616ce1178d8e75bddc9a8f5a1
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-11-08 15:17:12 -0400

    feat: added MongoDB service

---
+++ mongodb/initdb.d/setup-users.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mongosh <<EOF
+use ${INTERNAL_FILE_STORAGE_MONGODB_DB}
+db.createUser({
+  user: "${INTERNAL_FILE_STORAGE_MONGODB_USERNAME}",
+  pwd: "${INTERNAL_FILE_STORAGE_MONGODB_PASSWORD}",
+  roles: [{ role: "dbOwner", db: "${INTERNAL_FILE_STORAGE_MONGODB_DB}" }]
+})
+
+use ${MOVIES_MONGODB_DB}
+db.createUser({
+  user: "${MOVIES_MONGODB_USERNAME}",
+  pwd: "${MOVIES_MONGODB_PASSWORD}",
+  roles: [{ role: "dbOwner", db: "${MOVIES_MONGODB_DB}" }]
+})
+EOF
