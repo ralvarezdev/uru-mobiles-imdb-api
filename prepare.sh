aad27b67a1f98ae6d176fbb24d38bb055aec6319
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-12-11 19:48:25 -0400

    refactor: added sql related repositories

--- prepare.sh
+++ prepare.sh
@@ -4,26 +4,26 @@
 mkdir -p ./postgres/initdb.d/sql
 
 # Create the folders if they don't exist
-mkdir -p ./auth/sql
-mkdir -p ./user/sql
-mkdir -p ./movies/sql
+mkdir -p ./sql-auth
+mkdir -p ./sql-user
+mkdir -p ./sql-movies
 
 # Copy SQL initialization files from Auth microservice to the postgres directory
-for file in ./auth/sql/*.sql; do
+for file in ./sql-auth/*.sql; do
     if [ -f "$file" ]; then
         cp "$file" "./postgres/initdb.d/sql/auth_$(basename "$file")"
     fi
 done
 
 # Copy SQL initialization files from User microservice to the postgres directory
-for file in ./user/sql/*.sql; do
+for file in ./sql-user/*.sql; do
     if [ -f "$file" ]; then
         cp "$file" "./postgres/initdb.d/sql/user_$(basename "$file")"
     fi
 done
 
 # Copy SQL initialization files from Movies microservice to the postgres directory
-for file in ./movies/sql/*.sql; do
+for file in ./sql-movies/*.sql; do
     if [ -f "$file" ]; then
         cp "$file" "./postgres/initdb.d/sql/movies_$(basename "$file")"
     fi
